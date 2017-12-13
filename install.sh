975cefacfc634846aadb40348336ce74060ae39c
MTJailed <marketing@jailed.ml>
2017-12-13 12:23:17 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,5 +9,25 @@ echo "LICENSED under the MIT License"
 echo " "
 
 echo "Installing..."
+echo "Installing dependencies..."
+if [[ $(which python) -ne 0 ]]; then
+   echo "This script depends on python"
+   exit 1
+fi
+if [[ $(which pip) -ne 0 ]]; then
+   echo "Installing dependency pip..."
+   sudo python -m easy_install pip
+fi
+echo "(re)installing dependency pymodule lief..."
+pip install lief
+echo "Setting up apibuster..."
+echo "Moving api buster into place..."
 cp APIBuster/apibuster /usr/local/bin/apibuster
+echo "Moving machoparse into place..."
+cp APIBuster/machoparse /usr/local/bin/machoparse
+echo "Setting permissions..."
 chmod +x /usr/local/bin/apibuster
+chmod +x /usr/local/bin/machoparse
+echo "Setting up data..."
+mkdir -p /usr/local/share/apibuster
+cp -r APIBuster/src /usr/local/share/apibuster/
