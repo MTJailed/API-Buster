5807c3f26a8accb4ecff277a907e620b8a633cbb
MTJailed <marketing@jailed.ml>
2017-12-13 10:49:05 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+echo "[ API BUSTER INSTALLER ]"
+echo "Created by Sem Voigtlander"
+echo "LICENSED under the MIT License"
+echo " "
+
+echo "Installing..."
+cp apibuster /usr/local/bin/apibuster
+chmod +x /usr/local/bin/apibuster
